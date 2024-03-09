f02a058c1d5329f6fc48ed963086f40288cd8335
Dharshana <74519795+dharshanag@users.noreply.github.com>
2024-03-09 13:04:02 -0800

    Update kaleido-spiral.py

--- kaleido-spiral.py
+++ kaleido-spiral.py
@@ -1,17 +1,32 @@
 import turtle
+import time
 from itertools import cycle
 
 colors = cycle(['red','orange','yellow','green','blue','purple'])
 
+start = time.time()
 
 def draw_circle(size,angle,shift):
+    global start
     turtle.pencolor(next(colors))
     turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
+    end = time.time()
+    elapsed = end-start
+
+    if (elapsed > 180):
+        size = 30
+        angle = 0
+        shift = 1
+        start = time.time()
+        turtle.reset()
+        turtle.pensize(4)
+        turtle.hideturtle
     draw_circle(size + 5, angle +1, shift+1)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
 turtle.pensize(4)
+turtle.hideturtle()
 draw_circle(30,0,1)
